1072225ccb97e59a90b118249fc9ec027ca8b21c
Gil <gilgindin11@gmail.com>
2019-05-30 02:36:54 +0300

    final commit for now

--- app/src/main/java/com/gil/heroapp/MainActivity.java
+++ app/src/main/java/com/gil/heroapp/MainActivity.java
@@ -1,14 +1,20 @@
 package com.gil.heroapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,7 +35,6 @@ public class MainActivity extends AppCompatActivity implements MyHeroAdapter.OnI
     public static final String TEXT_HEADER = "textHeader";
     public static final String DEFAULT = "N/A";
 
-
     private static final String TAG = "MainActivity";
     private RecyclerView mRecyclerView;
     private MyHeroAdapter mMyHeroAdapter;
@@ -38,6 +43,8 @@ public class MainActivity extends AppCompatActivity implements MyHeroAdapter.OnI
     private List allResults;
     private String photo;
     private String nameText;
+    private SharedPreferences mSharedPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,13 +53,36 @@ public class MainActivity extends AppCompatActivity implements MyHeroAdapter.OnI
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.collapse_activity_main);
 
+        initialViews();
 
-        initialButtons();
-        createRetrofitCallback();
+        //checking internet connection , if needed move the user to internet settings
+        if (isOnline()) {
+            createRetrofitCallback();
+        } else {
+            startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+            Toast.makeText(this, "No internet connection", Toast.LENGTH_SHORT).show();
+        }
 
     }
 
-    private void initialButtons() {
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        if (isOnline()) {
+            createRetrofitCallback();
+        } else startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isOnline()) {
+            createRetrofitCallback();
+        } else startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+    }
+
+    //initial the views on MainActivity
+    private void initialViews() {
         mRecyclerView = findViewById(R.id.recycler_view_list_of_heroes);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.setHasFixedSize(true);
@@ -62,6 +92,7 @@ public class MainActivity extends AppCompatActivity implements MyHeroAdapter.OnI
 
     }
 
+    //creating Retrofit callBack to HeroApp api
     private void createRetrofitCallback() {
         EndPointService mEndPointService = RetrofitInstance.getRetrofitInstance().create(EndPointService.class);
         Call<List<Item>> myCall = mEndPointService.getAllHeroes();
@@ -82,9 +113,7 @@ public class MainActivity extends AppCompatActivity implements MyHeroAdapter.OnI
                 }
 
                 mMyHeroAdapter = new MyHeroAdapter(MainActivity.this, allResults);
-                mRecyclerView.setAdapter(mMyHeroAdapter);
-                mMyHeroAdapter.setOnItemClickListener(MainActivity.this);
-                mMyHeroAdapter.notifyDataSetChanged();
+                runAnimation(mRecyclerView, 0);
 
             }
 
@@ -96,6 +125,22 @@ public class MainActivity extends AppCompatActivity implements MyHeroAdapter.OnI
         });
     }
 
+    //animiation for the recyclerview list at upload
+    private void runAnimation(RecyclerView recyclerView, int type) {
+
+        Context context = mRecyclerView.getContext();
+        LayoutAnimationController controller = null;
+
+        if (type == 0) {
+            controller = AnimationUtils.loadLayoutAnimation(context, R.anim.layout_slider_from_left);
+            setAdpter();
+            mRecyclerView.setLayoutAnimation(controller);
+
+        }
+    }
+
+
+    //function override from the recyclerview adapter , get item clicked position
     @Override
     public void onItemClick(int position) {
         Item clieckedItem = (Item) allResults.get(position);
@@ -103,17 +148,19 @@ public class MainActivity extends AppCompatActivity implements MyHeroAdapter.OnI
         nameText = clieckedItem.getTitle();
 
         saveToSharedPrefs();
-        swapItem(position, 0);
         updateAppHeader(photo, nameText);
-
+        swapItem(position, 0);
     }
 
-    public void swapItem(int fromPostion, int toPosition) {
+    //function override from the recyclerview adpater , moving the clicked item to top of the list
+    public void swapItem(int fromPostion, final int toPosition) {
         Collections.swap(allResults, fromPostion, toPosition);
         mMyHeroAdapter.notifyItemMoved(fromPostion, toPosition);
-        mRecyclerView.scrollToPosition(toPosition);
+        mRecyclerView.smoothScrollToPosition(mMyHeroAdapter.getItemCount());
     }
 
+    //function override from the recyclerview adapter , ypload the header image and text with
+    //current item clicked's image and text
     public void updateAppHeader(String photo, String name) {
         if (photo != null) {
             Picasso.with(MainActivity.this).load(photo).fit().into(mImageView);
@@ -121,19 +168,21 @@ public class MainActivity extends AppCompatActivity implements MyHeroAdapter.OnI
         mTextView.setText(nameText);
     }
 
+    //save image and text header to sharedPreferences
     private void saveToSharedPrefs() {
-        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
+        mSharedPreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
 
         editor.putString(IMAGE_HEADR, photo);
         editor.putString(TEXT_HEADER, nameText);
         editor.commit();
     }
 
+    //load data through sharedPreferences
     private void loadSharedPrefs() {
-        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
-        String loadPhoto = sharedPreferences.getString(IMAGE_HEADR, DEFAULT);
-        String loadNameText = sharedPreferences.getString(TEXT_HEADER, DEFAULT);
+        mSharedPreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        String loadPhoto = mSharedPreferences.getString(IMAGE_HEADR, DEFAULT);
+        String loadNameText = mSharedPreferences.getString(TEXT_HEADER, DEFAULT);
 
         if (loadNameText.equals(DEFAULT) || loadPhoto.equals(DEFAULT)) {
 
@@ -146,5 +195,20 @@ public class MainActivity extends AppCompatActivity implements MyHeroAdapter.OnI
         }
     }
 
+    //internet connection check
+    public boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+
+    //set the adapter with the recyclerview
+    public void setAdpter() {
+        mRecyclerView.setAdapter(mMyHeroAdapter);
+        mMyHeroAdapter.setOnItemClickListener(MainActivity.this);
+        mMyHeroAdapter.notifyDataSetChanged();
+
+    }
 
 }
